cf7ca992cebb569b20440d125951039481eac579
lucaszamorapy <lucaszamora.zulmira@gmal.com>
2024-03-12 17:44:19 -0300

    feat: useForm

--- src/components/loginForm/LoginForm.jsx
+++ src/components/loginForm/LoginForm.jsx
@@ -1,36 +1,51 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useFormAction } from "react-router-dom";
 import Input from "../../utils/input/Input";
 import Button from "../../utils/button/Button";
+import UseForm from "../../hooks/useform/UseForm";
 
 const LoginForm = () => {
-  const [username, setUsername] = React.useState("");
-  const [password, setPassword] = React.useState("");
+  const username = UseForm(); // Colocando em uma variavel o hook, e por utilizar no handlesubmit
+  const password = UseForm(); // Colocando em uma variavel o hook, e por utilizar no handlesubmit
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username, password }),
-    })
-      .then((response) => {
-        console.log(response);
-        return response.json();
+
+    if (username.validate() && password.validate()) {
+      // Se o username e password for true ele vai fazer o fetch
+      fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
+        // Faz uma requisição para a URL especificada
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json", // Especifica o tipo de conteúdo da requisição como JSON
+        },
+        body: JSON.stringify(), //Corpo da requisição, convertendo o objeto { username, password } para uma string JSON
       })
-      .then((json) => {
-        console.log(json);
-      });
+        .then((response) => {
+          // Manipulação da resposta da requisição
+          console.log(response); // Exibe a resposta da requisição no console
+          return response.json(); // Converte a resposta da requisição para JSON e retorna a promessa resultante
+        })
+        .then((json) => {
+          // Manipulação dos dados JSON retornados pela promessa anterior
+          console.log(json); // Exibe os dados JSON no console
+        });
+    }
   };
 
   return (
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input label="Usuário" type="text" name="username" />
-        <Input label="Senha" type="password" name="password" />
+        <Input label="Usuário" type="text" name="username" {...username} />{" "}
+        {/* Colocando todos os props do Hook no input */}
+        <Input
+          label="Senha"
+          type="password"
+          name="password"
+          {...password}
+        />{" "}
+        {/* Colocando todos os props do Hook no input */}
         <Button>Entrar</Button>
       </form>
       <Link to="/login/criar">Cadastro</Link>
