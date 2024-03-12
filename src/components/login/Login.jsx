b08dbfddeb8069a7ab12c2062c6c589b50a89b26
lucaszamorapy <lucaszamora.zulmira@gmal.com>
2024-03-12 14:28:38 -0300

    feat: login routes

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import LoginForm from "../loginForm/LoginForm";
+import LoginCreate from "../loginCreate/LoginCreate";
+import LoginPasswordReset from "../loginPasswordReset/LoginPasswordReset";
+import LoginPasswordLost from "../loginPasswordLost/LoginPasswordLost";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="criar" element={<LoginCreate />} />
+        <Route path="perdeu" element={<LoginPasswordLost />} />
+        <Route path="resetar" element={<LoginPasswordReset />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default Login;
