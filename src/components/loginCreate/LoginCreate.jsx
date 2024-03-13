f9fb1dcfa8fb46bae6732bc60c87dfc5386eb3b3
lucaszamorapy <lucaszamora.zulmira@gmal.com>
2024-03-13 17:59:54 -0300

    feat: validação cadastro

--- src/components/loginCreate/LoginCreate.jsx
+++ src/components/loginCreate/LoginCreate.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import Input from "../../utils/input/Input";
+import Button from "../../utils/button/Button";
+import useForm from "../../hooks/useform/UseForm";
 
 const LoginCreate = () => {
-  return <div>LoginCreate</div>;
+  const username = useForm();
+  const email = useForm("email");
+  const password = useForm("password");
+
+  const criarUsuario = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <section className="animeLeft">
+      <h1 className="title">Cadastre-se</h1>
+      <form onSubmit={criarUsuario}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Email" type="email" name="email" {...email} />
+        <Input label="Senha" type="password" name="username" {...password} />
+        <Button>Cadastrar</Button>
+      </form>
+    </section>
+  );
 };
 
 export default LoginCreate;
