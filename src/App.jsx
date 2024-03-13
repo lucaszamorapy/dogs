b9756070017754420e15e9197bc71dc6f668f866
lucaszamorapy <lucaszamora.zulmira@gmal.com>
2024-03-13 13:36:13 -0300

    feat: userContext

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,20 @@ import Footer from "./components/footer/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import { UserStorage } from "./UserContext";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="login/*" element={<Login />} />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
--- src/components/loginForm/LoginForm.jsx
+++ src/components/loginForm/LoginForm.jsx
@@ -3,49 +3,29 @@ import { Link, useFormAction } from "react-router-dom";
 import Input from "../../utils/input/Input";
 import Button from "../../utils/button/Button";
 import UseForm from "../../hooks/useform/UseForm";
+import { TOKEN_POST, USER_GET } from "../../api";
+import { UserContext } from "../../UserContext";
 
 const LoginForm = () => {
-  const username = UseForm(); // Colocando em uma variavel o hook, e por utilizar no handlesubmit
-  const password = UseForm(); // Colocando em uma variavel o hook, e por utilizar no handlesubmit
+  const username = UseForm();
+  const password = UseForm();
 
-  const handleSubmit = (event) => {
+  const { userLogin } = React.useContext(UserContext);
+
+  async function handleSubmit(event) {
     event.preventDefault();
 
     if (username.validate() && password.validate()) {
-      // Se o username e password for true ele vai fazer o fetch
-      fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
-        // Faz uma requisição para a URL especificada
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json", // Especifica o tipo de conteúdo da requisição como JSON
-        },
-        body: JSON.stringify(), //Corpo da requisição, convertendo o objeto { username, password } para uma string JSON
-      })
-        .then((response) => {
-          // Manipulação da resposta da requisição
-          console.log(response); // Exibe a resposta da requisição no console
-          return response.json(); // Converte a resposta da requisição para JSON e retorna a promessa resultante
-        })
-        .then((json) => {
-          // Manipulação dos dados JSON retornados pela promessa anterior
-          console.log(json); // Exibe os dados JSON no console
-        });
+      userLogin(username.value, password.value);
     }
-  };
+  }
 
   return (
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input label="Usuário" type="text" name="username" {...username} />{" "}
-        {/* Colocando todos os props do Hook no input */}
-        <Input
-          label="Senha"
-          type="password"
-          name="password"
-          {...password}
-        />{" "}
-        {/* Colocando todos os props do Hook no input */}
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
         <Button>Entrar</Button>
       </form>
       <Link to="/login/criar">Cadastro</Link>
