9befe9a950e187bdcc823072f579c06e0e76bb64
lucaszamorapy <lucaszamora.zulmira@gmal.com>
2024-03-14 10:48:59 -0300

    feat: rota protegida

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import { UserStorage } from "./UserContext";
+import User from "./pages/user/User";
+import ProtectedRouter from "./helper/protectedRouter/ProtectedRouter";
 
 const App = () => {
   return (
@@ -16,6 +18,14 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="login/*" element={<Login />} />
+            <Route
+              path="conta/*"
+              element={
+                <ProtectedRouter>
+                  <User />
+                </ProtectedRouter>
+              }
+            />
           </Routes>
           <Footer />
         </UserStorage>
