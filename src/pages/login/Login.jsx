d0833114868cb8f5f79e2c1ed9473ffdbbbaadc1
Lucas Zamora <113631100+lucaszamorapy@users.noreply.github.com>
2024-03-31 14:38:10 -0300

    feat: password  reset

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -11,7 +11,7 @@ import NotFound from "../../components/notfound/NotFound";
 const Login = () => {
   const { login } = React.useContext(UserContext);
 
-  if (login === true) return <Navigate to="/conta" />;
+  if (login === true) return <Navigate to="/conta" />; //Se o login entrar, redirecionar para /conta
   return (
     <section className={styles.login}>
       <div className={styles.forms}>
--- src/pages/login/loginPasswordLost/LoginPasswordLost.jsx
+++ src/pages/login/loginPasswordLost/LoginPasswordLost.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import Input from "../../../utils/input/Input";
+import Button from "../../../utils/button/Button";
+import UseForm from "../../../hooks/useform/UseForm";
+import useFetch from "../../../hooks/usefetch/useFetch";
+import Erro from "../../../helper/erro/Erro";
+import { PASSWORD_LOST } from "../../../api";
 
 const LoginPasswordLost = () => {
-  return <div>LoginPasswordLost</div>;
+  const login = UseForm();
+  const { data, loading, error, request } = useFetch();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (login.validate());
+    const { url, options } = PASSWORD_LOST({
+      login: login.value,
+      url: window.location.href.replace("perdeu", "resetar"),
+    });
+    const { json } = await request(url, options);
+    console.log(json);
+  };
+
+  return (
+    <section>
+      <h1 className="title">Perdeu a senha?</h1>
+      {data ? (
+        <p style={{ color: "#4c1" }}>{data}</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <Input
+            label={"Email / Usuário"}
+            type="text"
+            name="login"
+            {...login}
+          />
+          {loading ? (
+            <Button disabled>Enviando...</Button>
+          ) : (
+            <Button>Enviar email</Button>
+          )}
+        </form>
+      )}
+      <Erro error={error} />
+    </section>
+  );
 };
 
 export default LoginPasswordLost;
